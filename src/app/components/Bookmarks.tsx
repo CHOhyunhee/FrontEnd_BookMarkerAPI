fd4224b9d5ada803d399cbba535391bb52b346a5
zzee1 <www9095@gmail.com>
2025-02-20 16:32:37 +0900

    add Bookmark Component

--- src/app/components/Bookmarks.tsx
+++ src/app/components/Bookmarks.tsx
@@ -1,5 +1,6 @@
 import { BookmarkType } from '@/types';
 import React from 'react';
+import Bookmark from '@/app/components/Bookmark';
 
 type Props = {
     bookmarks: BookmarkType[]
@@ -10,11 +11,11 @@ const Bookmarks: React.FC<Props> = ({bookmarks}) => {
         <div>
             <h1>Welcome to Bookmarker</h1>
             {
-            bookmarks.map(bookmark => {
-                return (
-                    <h2 key={bookmark.id}>* {bookmark.title}</h2>
-                )
-            })
+            // key: virtual dom (react내부적으로 화면 rendering) 때문에..
+            // 바뀐 부분만 빠르게 update...
+            bookmarks.map(bookmark => 
+                <Bookmark key={bookmark.id} bookmark={bookmark} />
+            )
             }            
         </div>
     );
