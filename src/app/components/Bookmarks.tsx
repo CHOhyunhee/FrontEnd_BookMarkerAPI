d0ab31e26eca09da1594a76ae8b2fb0bf03bb61d
zzee1 <www9095@gmail.com>
2025-02-20 16:48:16 +0900

    Pagination

--- src/app/components/Bookmarks.tsx
+++ src/app/components/Bookmarks.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BookmarksResponse } from '@/types';
 import Bookmark from '@/app/components/Bookmark';
+import Pagination from '@/app/components/Pagination';
 
 type Props = {
     bookmarks: BookmarksResponse
@@ -9,13 +10,12 @@ type Props = {
 const Bookmarks: React.FC<Props> = ({bookmarks}) => {
     return (
         <div>
-            <h1>Welcome to Bookmarker</h1>
+            <Pagination bookmarks={bookmarks} />
             {
             // key: virtual dom (react내부적으로 화면 rendering) 때문에..
             // 바뀐 부분만 빠르게 update...
             bookmarks.data.map(bookmark => 
-                <Bookmark key={bookmark.id} bookmark={bookmark} />
-            )
+                <Bookmark key={bookmark.id} bookmark={bookmark} />)
             }            
         </div>
     );
