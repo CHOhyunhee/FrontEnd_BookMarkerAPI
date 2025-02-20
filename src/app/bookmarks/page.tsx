4b68dd8bdf2b005e52e2f86697895ff41ed33d2e
zzee1 <www9095@gmail.com>
2025-02-20 15:49:20 +0900

    redirect

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,8 +1,20 @@
-const page = () => (
-    <div>
+
+export default function BookmarkPage()  {
+    //distructuring assignment
+    const { titles } = getData()
+    
+    return (
+      <div>
           <h1>Bookmarks Home</h1>
+          {
+            titles.map((title,idx) => <h2 key={idx}>{title}</h2>)
+          }
+      </div>
+    );
+  }
   
-    </div>
-  );
-  
-  export default page;
+  function getData()  {
+    return {
+        titles: ['Bookmark1', 'Bookmark2']
+    }
+  }
