520f1583e06517a06486af0b3c802a21d4acd97a
zzee1 <www9095@gmail.com>
2025-02-20 17:40:47 +0900

    searchForm 추가

--- src/app/bookmarks/add/page.tsx
+++ src/app/bookmarks/add/page.tsx
@@ -1,15 +1,50 @@
 'use client'
 
 import React, {useState} from "react";
+import { useRouter } from "next/navigation";
+import { saveBookmark } from "@/lib/bookmarkApi";
 
 const AddBookmarkPage = () => {
     const [title, setTitle] = useState("");
     const [url, setUrl] = useState("");
     const [message, setMessage] = useState<string|null>(null);
 
-    const handleSubmit = async (e: React.SyntheticEvent) => {
+    const router = useRouter();
 
-     }
+    const handleSubmit = async (e: React.SyntheticEvent) => {
+        //기본 event 동작 막음 
+        e.preventDefault();
+        
+        if(!url) {
+          alert("Please enter URL");
+          return;
+        }
+
+        if(!title) {
+          alert("Please enter Title");
+          return;
+        }
+
+        const payload = { //payload object에 입력한 값 설정
+           title, url
+        }
+        
+        try {
+          const response = await saveBookmark(payload);
+          console.log("SaveBookmark response: ", response);
+          setMessage("Bookmark saved successfully");  
+          await router.push(`/bookmarks`);
+          router.refresh();
+        } catch (error) {
+          console.log('등록 실패!!!!')  
+          console.log(error);
+          setMessage("Bookmark saved fail");  
+        }
+    
+        setTitle("");
+        setUrl("");
+            
+    } //handleSubmit 
 
     return (
       <div>
